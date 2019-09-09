9bcdfb39f11f6d3b051007a9ea7447259b5b806e
Isala Piyarisi <code@isala.me>
2019-09-09 21:54:15 +0530

    Added material-ui react-router

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class SideBar extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default SideBar;
