be258bfdb71ee9026e7498ddf86bc3623cc9a1e0
Isala Piyarisi <code@isala.me>
2019-09-11 13:12:27 +0530

    Code Destructuring

--- src/App.js
+++ src/App.js
@@ -1,55 +1,13 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Drawer from '@material-ui/core/Drawer';
 import Hidden from '@material-ui/core/Hidden';
-import {makeStyles} from '@material-ui/core/styles';
-import SideBar from "./components/SideBar";
 import Header from "./components/Header";
 import Home from "./views/Home";
-import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import NavBar from "./components/NavBar";
+import {useStyles, theme} from './Theme'
 
-const theme = createMuiTheme({
-    palette: {
-        type: 'dark'
-    },
-});
-
-const drawerWidth = 240;
-
-const useStyles = makeStyles(theme => ({
-    root: {
-        display: 'flex'
-    },
-    drawer: {
-        [theme.breakpoints.up('sm')]: {
-            width: drawerWidth,
-            flexShrink: 0,
-        },
-    },
-    appBar: {
-        marginLeft: drawerWidth,
-        [theme.breakpoints.up('sm')]: {
-            width: `calc(100% - ${drawerWidth}px)`,
-        },
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-        [theme.breakpoints.up('sm')]: {
-            display: 'none',
-        },
-    },
-    toolbar: theme.mixins.toolbar,
-    drawerPaper: {
-        width: drawerWidth,
-    },
-    content: {
-        flexGrow: 1,
-        padding: theme.spacing(3),
-    },
-}));
-
-function App(props) {
-    const {container} = props;
+const App = () => {
     const classes = useStyles();
     const [mobileOpen, setMobileOpen] = React.useState(false);
 
@@ -64,37 +22,7 @@ function App(props) {
             <Hidden smUp implementation="css">
                 <Header classes={classes} handleDrawerToggle={handleDrawerToggle}/>
             </Hidden>
-            <nav className={classes.drawer} aria-label="mailbox folders">
-                {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-                <Hidden smUp implementation="css">
-                    <Drawer
-                        container={container}
-                        variant="temporary"
-                        anchor="left"
-                        open={mobileOpen}
-                        onClose={handleDrawerToggle}
-                        classes={{
-                            paper: classes.drawerPaper,
-                        }}
-                        ModalProps={{
-                            keepMounted: true, // Better open performance on mobile.
-                        }}
-                    >
-                        <SideBar toolbar={classes.toolbar}/>
-                    </Drawer>
-                </Hidden>
-                <Hidden xsDown implementation="css">
-                    <Drawer
-                        classes={{
-                            paper: classes.drawerPaper,
-                        }}
-                        variant="permanent"
-                        open
-                    >
-                        <SideBar toolbar={classes.toolbar}/>
-                    </Drawer>
-                </Hidden>
-            </nav>
+            <NavBar classes={classes} handleDrawerToggle={handleDrawerToggle} mobileOpen={mobileOpen}  />
             <main className={classes.content}>
                 <Hidden smUp implementation="css">
                     <div className={classes.toolbar}/>
---
+++ src/Theme.js
@@ -0,0 +1,41 @@
+import {createMuiTheme, makeStyles} from "@material-ui/core";
+
+export const theme = createMuiTheme({
+    palette: {
+        type: 'dark'
+    },
+});
+
+const drawerWidth = 240;
+
+export const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex'
+    },
+    drawer: {
+        [theme.breakpoints.up('sm')]: {
+            width: drawerWidth,
+            flexShrink: 0,
+        },
+    },
+    appBar: {
+        marginLeft: drawerWidth,
+        [theme.breakpoints.up('sm')]: {
+            width: `calc(100% - ${drawerWidth}px)`,
+        },
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+        [theme.breakpoints.up('sm')]: {
+            display: 'none',
+        },
+    },
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+    },
+}));
