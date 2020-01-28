584d51c2b87f132be88818adbbf28d1b1e68d0c9
Isala Piyarisi <code@isala.me>
2020-01-28 21:29:37 +0530

    Changed NodeJs to Typescript

--- src/components/Resume/Skills/Backend.jsx
+++ src/components/Resume/Skills/Backend.jsx
@@ -7,7 +7,7 @@ const Backend = (props) => {
             <h3 className={"skill-heading"}>Back-End</h3>
             <Skill name={"Python"} icon={"python.svg"}/>
             <Skill name={"GoLang"} icon={"golang.svg"}/>
-            <Skill name={"NodeJS"} icon={"nodejs.svg"}/>
+            <Skill name={"TypeScript"} icon={"typescript.svg"}/>
             <Skill name={"Java"} icon={"java.svg"}/>
             <Skill name={"PHP"} icon={"php.svg"}/>
         </div>
