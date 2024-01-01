b92e4d7376735d633a689bc3f124eea615da1164
Isala Piyarisi <code@isala.me>
2024-01-02 01:28:33 +0530

    Add Image enhancements (#34)

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { enhancedImages } from '@sveltejs/enhanced-img';
 
 export default defineConfig({
 	optimizeDeps: {
 	  include: ['lodash.get', 'lodash.isequal', 'lodash.clonedeep']
 	},
-	plugins: [sveltekit()]
+	plugins: [enhancedImages(), sveltekit()]
 })
